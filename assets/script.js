b4fe23d0334b3668b655db7e2345e437888ee7bb
Krystyn Smith <krskrsmith@gmail.com>
2021-08-22 13:02:02 -0500

    working on some styling and getting the program to check the current time against the listed times

--- assets/script.js
+++ assets/script.js
@@ -8,17 +8,27 @@ function displayDate() {
     timeDisplay.text(rightNow);
   }
 
+//function to check the current time against the calendar time so I can color code the bars
+//seems to only be running the code against actual current time console.logging 'past' and 'current' only
 function colorTimeBars() {
-    var calTime = calendarTime
-    if (displayDate > calTime) {
+    //var calTime = calendarTime.text();
+    if (displayDate > calendarTime) {
         console.log("past time");
-} else if (displayDate < calTime) {
+        console.log(calendarTime);
+} else if (displayDate < calendarTime) {
     console.log("future time");
-} else (displayDate == calTime); {
+    console.log(calendarTime);
+} else (displayDate == calendarTime); {
     console.log("current time");
+    console.log(calendarTime);
 }
 }
+//if applicable... these classes color the cells. possibly use in the loop?
+//<tr class="table-success">...</tr>
+//<tr class="table-danger">...</tr>
+//<tr class="table-dark">...</tr>
 
-colorTimeBars();
 
-  setInterval(displayDate, 1000);
+colorTimeBars(); //running the time check code to color the bars
+
+  setInterval(displayDate, 1000); //running the clock
